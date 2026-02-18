b4657b7b691c9d62568349d46ca4c1e29718f69c
Sandip Musale <sandipmusale24@gmail.com>
2026-02-18 21:25:27 +0530

    Initial Commit

---
+++ GitHub Repositories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Repositories
+//
+//  Created by Sandip Musale on 18/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Repositories/GitHub_RepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_RepositoriesApp.swift
+//  GitHub Repositories
+//
+//  Created by Sandip Musale on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_RepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
